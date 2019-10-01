84812eaf986e7581e98a3dadea992bdbe9b56719
David Terkula <david.terkula@kroger.com>
2019-09-30 20:21:53 -0400

    remote db on aws rds

--- src/main/kotlin/com/terkula/uaxctf/statistics/controller/SeasonBestController.kt
+++ src/main/kotlin/com/terkula/uaxctf/statistics/controller/SeasonBestController.kt
@@ -8,7 +8,7 @@ import java.sql.Date
 import java.util.stream.IntStream
 import kotlin.streams.toList
 
-@RestController("/")
+@RestController()
 class SeasonBestController(@field:Autowired
                    internal var seasonBestService: SeasonBestService) {
 
