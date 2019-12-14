ffa238ac6c5be299356882e8a2a9d747e6f6d8cf
dt46014 <david.terkula@kroger.com>
2019-12-14 16:50:02 -0500

    adding eqauls method to Runner class to allow proper groupBy clauses

--- src/main/kotlin/com/terkula/uaxctf/statisitcs/model/Runner.kt
+++ src/main/kotlin/com/terkula/uaxctf/statisitcs/model/Runner.kt
@@ -18,4 +18,26 @@ class Runner(
     override fun toString(): String {
         return this.name + ": " + graduatingClass
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Runner
+
+        if (name != other.name) return false
+        if (graduatingClass != other.graduatingClass) return false
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + graduatingClass.hashCode()
+        result = 31 * result + id
+        return result
+    }
+
+
 }
