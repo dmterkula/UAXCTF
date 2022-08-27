5278d8340c8db6bc8dbf35d3730abd8a69a92de8
dmterkula <dmterkula@gmail.com>
2022-08-27 14:40:49 -0400

    serialize sb and prs as result to make a common data model possible in ios app

--- src/main/kotlin/com/terkula/uaxctf/statistics/dto/PRDTO.kt
+++ src/main/kotlin/com/terkula/uaxctf/statistics/dto/PRDTO.kt
@@ -1,5 +1,6 @@
 package com.terkula.uaxctf.statistics.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.terkula.uaxctf.statisitcs.model.Runner
 
-class PRDTO (val runner: Runner, val pr: List<MeetPerformanceDTO>, val improvedUpon: ImprovedUponDTO)
+class PRDTO (val runner: Runner, @JsonProperty("result") val pr: List<MeetPerformanceDTO>, val improvedUpon: ImprovedUponDTO)
--- src/main/kotlin/com/terkula/uaxctf/statistics/dto/SeasonBestDTO.kt
+++ src/main/kotlin/com/terkula/uaxctf/statistics/dto/SeasonBestDTO.kt
@@ -1,5 +1,6 @@
 package com.terkula.uaxctf.statistics.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.terkula.uaxctf.statisitcs.model.Runner
 
-class SeasonBestDTO (val runner: Runner, val seasonBest: List<MeetPerformanceDTO>, val improvedUpon: ImprovedUponDTO)
+class SeasonBestDTO (val runner: Runner, @JsonProperty("result") val seasonBest: List<MeetPerformanceDTO>, val improvedUpon: ImprovedUponDTO)
