1d657847f8f19033c8e702bf1d8d8d256eb9c75d
dmterkula <dmterkula@gmail.com>
2022-10-05 14:08:08 -0400

    change active from val to var to allow change in default on runner creation request

--- src/main/kotlin/com/terkula/uaxctf/statistics/request/CreateRunnerRequest.kt
+++ src/main/kotlin/com/terkula/uaxctf/statistics/request/CreateRunnerRequest.kt
@@ -1,3 +1,3 @@
 package com.terkula.uaxctf.statistics.request
 
-class CreateRunnerRequest(val name: String, val graduatingClass: String, val isActive: Boolean = true)
+class CreateRunnerRequest(val name: String, val graduatingClass: String, var isActive: Boolean = true)
